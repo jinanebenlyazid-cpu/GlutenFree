bcf1707937c73726a1c324e2cc8023733f138aa6
Jinane <jinanebenliazid@gmail.com>
2026-01-15 18:13:21 +0100

    add

--- routes/web.php
+++ routes/web.php
@@ -26,59 +26,7 @@ Route::fallback(function () {
     return view('erreur'); 
 });
 
-// Route::get('/produits/{cat}', function ($cat) { 
-//     $produits = []; 
 
-//     if ($cat == 'pains-viennoiseries') { 
-//         $produits =[
-//         'Pain complet sans gluten' => [
-//             'description' => 'Pain riche en fibres, bio',
-//             'prix' => 15.50,
-//             'image' => '/images/produits/pain_complet.jpg'
-//         ],
-//         'Croissant sans gluten' => [
-//             'description' => 'Croissant léger et croustillant',
-//             'prix' => 3.00,
-//             'image' => '/images/produits/croissant.jpg'
-//         ],
-//     ]; 
-//     } elseif ($cat == 'cereales-farines') { 
-//         $produits = [
-//         'Farine de riz bio' => [
-//             'description' => 'Idéale pour pâtisserie et pain sans gluten',
-//             'prix' => 72,
-//             'image' => '/images/produits/farine_riz.jpg'
-//         ],
-//         'Flocons d’avoine sans gluten' => [
-//             'description' => 'Pour petit-déjeuner sain',
-//             'prix' => 69,
-//             'image' => '/images/produits/flocons_avoine.jpg'
-//         ],
-//     ]; 
-//     } 
-//      elseif ($cat == 'biscuits-snacks') { 
-//         $produits = [
-//         'Cookies chocolat sans gluten' => [
-//             'description' => 'Cookies croustillants, bio',
-//             'prix' => 6.50,
-//             'image' => '/images/produits/cookies.jpg'
-//         ],
-//         'Barres énergétiques sans gluten' => [
-//             'description' => 'Snack sain pour la randonnée',
-//             'prix' => 4.00,
-//             'image' => '/images/produits/barres.jpg'
-//         ],
-//     ]; 
-//     } 
-//     else{
-//         return view('erreur');
-//     }
- 
-//     return view('Produits', [ 
-//         'products' => $produits, 
-//         'categorie' => $cat 
-//     ]); 
-// }); 
 
 Route::get('/produits', function () { 
     $produits = [
